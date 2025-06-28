cc69e09c343ce365a5db4c82ffdd2db9374560fb
jdmontes <155933759+jdmontes2009@users.noreply.github.com>
2025-06-27 20:07:01 -0700

    Minor Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;                
 import 'package:maps_launcher/maps_launcher.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 
